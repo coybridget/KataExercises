4ee300d9fcee9825956e01f801b6eea482b8db08
Bridget Coy <coybridget@gmail.com>
2017-05-02 12:21:16 -0400

    driverlisence kata/code/test

---
+++ KataExercises/DriverLicense.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataExercises
+{     /*
+    * In order to receive a drivers license in the state of ohio you need to have
+    * received 50 hours of supervised driving practice, passed a vision exam, and a written exam
+    * if you are under the age of 18 years old. If you are over 18 years of age you need to have
+    * passed the vision exam and a written exam. If you are under 16 years old you are not allowed
+    * to legally drive.
+    * 
+    * GetDriversLicense(16, 20, false, true) - false
+    * GetDriversLicense(16, 50, false, false) - false
+    * GetDriversLicense(16, 50, true, true) - true
+    * GetDriversLicense(18, 0, true, true) - true
+    * 
+    */
+    public class DriverLicense
+    {
+   
+        public bool GetDriversLicense(int age, int hoursOfDrivingPractice, bool passedVision, bool passedWritten)
+        {
+            if(age < 16)
+            {
+                return false;
+            }
+
+            if(age < 18 &&  hoursOfDrivingPractice <= 50 || (passedVision =! true) || (passedWritten =! true))
+            {
+                return false;
+            }
+            if (age > 18 && (passedVision = !true) || (passedWritten = !true))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ KataExercisesTests/DriverLicenseTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KataExercises;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataExercises.Tests
+{
+    [TestClass()]
+    public class DriverLicenseTests
+    {
+        [TestMethod()]
+        public void GetDriversLicenseTestFalse()
+        {
+            //arrange
+            DriverLicense driverLicense = new DriverLicense();
+
+            //act
+            bool result = driverLicense.GetDriversLicense(16, 20, false, true);//false
+
+            bool result1 = driverLicense.GetDriversLicense(16, 50, false, false);//false
+
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(false, result1);
+
+        }
+
+        [TestMethod()]
+        public void GetDriversLicenseTestTrue()
+        {
+            //arrange
+            DriverLicense driverLicense = new DriverLicense();
+
+            bool result2 = driverLicense.GetDriversLicense(16, 50, true, true);//true
+
+            bool result3 = driverLicense.GetDriversLicense(18, 0, true, true);//true
+
+            Assert.AreEqual(true, result2);
+            Assert.AreEqual(true, result3);
+
+        }
+    }
+}
